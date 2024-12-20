2a0ac19a3c0811136565a82e2bc6c08bc3e64993
replay9zz <xiexiecuisite@gmail.com>
2024-12-21 02:14:37 +0900

    Add a guide for vistors in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,47 @@
 // src/app/page.tsx
 'use client';
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import RegularTetrahedron from '@/components/RegularTetrahedron';
 import HoverInfoDisplay from '@/components/HoverInfoDisplay';
 import '@/app/styles.css';
 
 const Home = () => {
-  const [hoveredUrl, setHoveredUrl] = useState('');
+ const [hoveredUrl, setHoveredUrl] = useState('');
+ const [isVisible, setIsVisible] = useState(false);
 
-  return (
-    <div className="fixed inset-0 overflow-hidden">
-      <div className="relative h-full w-full">
-        <RegularTetrahedron setHoveredUrl={setHoveredUrl} />
-        <div className="absolute top-20 w-full text-center">
-          <HoverInfoDisplay text={hoveredUrl} />
-        </div>
-        <h1 className="title">&quot;HOME&quot;</h1>
-      </div>
-    </div>
-  );
+ useEffect(() => {
+   // /aboutなどからの遷移かチェック
+   const referrer = document.referrer;
+   const isInternalNavigation = referrer.includes(window.location.origin);
+
+   if (!isInternalNavigation) {
+     setIsVisible(true);
+     const timer = setTimeout(() => {
+       setIsVisible(false);
+     }, 3000);
+     return () => clearTimeout(timer);
+   }
+ }, []);
+
+ return (
+   <div className="fixed inset-0 overflow-hidden">
+     <div className="relative h-full w-full">
+       <RegularTetrahedron setHoveredUrl={setHoveredUrl} />
+       <div className="absolute top-20 w-full text-center">
+         <HoverInfoDisplay text={hoveredUrl} />
+       </div>
+       <h1 className="title">&quot;HOME&quot;</h1>
+       {isVisible && (
+         <div className="absolute bottom-10 w-full pointer-events-none flex justify-center">
+           <div className="flex flex-col items-center space-y-4 bg-black/50 p-6 rounded-lg text-white animate-fadeOut">
+             <p className="text-lg font-medium">Click the vertices to begin</p>
+           </div>
+         </div>
+       )}
+     </div>
+   </div>
+ );
 };
 
 export default Home;
